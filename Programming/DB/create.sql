9ea32e6246c79b1608d8d1fe6196a9158cd07ab3
liinhhnt <linhnt.ict@gmail.com>
2023-08-11 13:55:09 +0700

    add sample data

--- Programming/DB/create.sql
+++ Programming/DB/create.sql
@@ -16,6 +16,8 @@ CREATE TABLE `dock` (
   PRIMARY KEY (`dockId`)
 ) ;
 
+INSERT INTO `dock` VALUES (1,'Hust Station','1st Dai Co Viet Street', 100, 0, 1000, 'https://th.bing.com/th/id/OIP.DfcFX9B00Y6I5Uw9Ckx5TQHaE8?w=288&h=192&c=7&r=0&o=5&dpr=1.6&pid=1.7&fbclid=IwAR0_LME57QWeJYPn1wvs_yI5lQd3PdnlZSUpy6ElbS3rl7-WbduUkpNl8rw'),(2,'Hoan Kiem Station','Trang Tien Street', 50, 0, 999,'https://th.bing.com/th/id/OIP.DfcFX9B00Y6I5Uw9Ckx5TQHaE8?w=288&h=192&c=7&r=0&o=5&dpr=1.6&pid=1.7&fbclid=IwAR0_LME57QWeJYPn1wvs_yI5lQd3PdnlZSUpy6ElbS3rl7-WbduUkpNl8rw'),(3,'To Hoang','67 To Hoang Lane', 30, 0, 500, 'https://th.bing.com/th/id/OIP.DfcFX9B00Y6I5Uw9Ckx5TQHaE8?w=288&h=192&c=7&r=0&o=5&dpr=1.6&pid=1.7&fbclid=IwAR0_LME57QWeJYPn1wvs_yI5lQd3PdnlZSUpy6ElbS3rl7-WbduUkpNl8rw');
+
 DROP TABLE IF EXISTS `bikeType`;
 CREATE TABLE bikeType (
     `typeId` INT PRIMARY KEY,
@@ -55,7 +57,7 @@ END;
 //
 DELIMITER ;
 
-
+INSERT INTO `bike` VALUES (1,1,'S001','99-G1 12345',false,'https://www.bad-bike.it/wp-content/uploads/2022/03/EVO_FAT_Green_Flou.jpg', 1), (2,2,'SE001','99-G1 12345',false,'https://www.bad-bike.it/wp-content/uploads/2022/03/EVO_FAT_Green_Flou.jpg', 1), (3,3,'T001','99-G1 12345',false,'https://www.bad-bike.it/wp-content/uploads/2022/03/EVO_FAT_Green_Flou.jpg', 2);
 
 
 

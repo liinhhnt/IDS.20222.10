b7a3b4344594a5d308f0585085dcb6bc39c3886c
btLong402 <111285054+btLong402@users.noreply.github.com>
2023-08-22 10:48:44 +0700

    new push

--- Programming/src/data_access_layer/bike/Bike_DAL.java
+++ Programming/src/data_access_layer/bike/Bike_DAL.java
@@ -37,60 +37,60 @@ public class Bike_DAL {
 			return 0;
 	}
 
-	public static Bike getBikeById(int bikeId) throws SQLException {
-
-		Connection connection = EcoBikeDB.getConnection();
-		Statement statement = connection.createStatement();
-		String query = String.format("select * from `bike` where bikeId =  %d", bikeId);
-		ResultSet result = statement.executeQuery(query);
-
-		if (result.next()) {
-			Bike bike = Bike.createBike(result.getInt("type"));
-			bike.setImgUrl(result.getString("imgUrl"));
-			bike.setType(result.getInt("type"));
-			bike.setDockId(result.getInt("dockId"));
-			bike.setBikeId(result.getInt("bikeId"));
-			bike.setBarCode(result.getString("barCode"));
-			bike.setLicensePlate(result.getString("licencePlate"));
-			bike.setBeingUsed(result.getBoolean("isBeingUsed"));
-
-			return bike;
-		}
-		return null;
-	}
-
-	public static ArrayList<Bike> getBikeListInDock(int dock_id) throws SQLException {
-		ArrayList<Bike> bikeList = new ArrayList<Bike>();
-
-		Connection connection = EcoBikeDB.getConnection();
-		Statement statement = connection.createStatement();
-
-		String query = String.format("select * from `bike` where dockId = %d and isBeingUsed = false", dock_id);
-		ResultSet result = statement.executeQuery(query);
-		while (result.next()) {
-			Bike bike = Bike.createBike(result.getInt("type"));
-			bike.setImgUrl(result.getString("imgUrl"));
-			bike.setType(result.getInt("type"));
-			bike.setDockId(result.getInt("dockId"));
-			bike.setBikeId(result.getInt("bikeId"));
-			bike.setBarCode(result.getString("barCode"));
-			bike.setLicensePlate(result.getString("licencePlate"));
-			bike.setBeingUsed(result.getBoolean("isBeingUsed"));
-
-			// get other attribute of e-bike
-			switch (result.getInt("bikeId")) {
-			case StandardEBike.BIKE_TYPE_VALUE:
-				getEBikeAttribute((StandardEBike) bike);
-				break;
-			default:
-				break;
-			}
-
-			bikeList.add(bike);
-		}
-
-		return bikeList;
-	}
+//	public static Bike getBikeById(int bikeId) throws SQLException {
+//
+//		Connection connection = EcoBikeDB.getConnection();
+//		Statement statement = connection.createStatement();
+//		String query = String.format("select * from `bike` where bikeId =  %d", bikeId);
+//		ResultSet result = statement.executeQuery(query);
+//
+//		if (result.next()) {
+//			Bike bike = Bike.createBike(result.getInt("type"));
+//			bike.setImgUrl(result.getString("imgUrl"));
+//			bike.setType(result.getInt("type"));
+//			bike.setDockId(result.getInt("dockId"));
+//			bike.setBikeId(result.getInt("bikeId"));
+//			bike.setBarCode(result.getString("barCode"));
+//			bike.setLicensePlate(result.getString("licencePlate"));
+//			bike.setBeingUsed(result.getBoolean("isBeingUsed"));
+//
+//			return bike;
+//		}
+//		return null;
+//	}
+//
+//	public static ArrayList<Bike> getBikeListInDock(int dock_id) throws SQLException {
+//		ArrayList<Bike> bikeList = new ArrayList<Bike>();
+//
+//		Connection connection = EcoBikeDB.getConnection();
+//		Statement statement = connection.createStatement();
+//
+//		String query = String.format("select * from `bike` where dockId = %d and isBeingUsed = false", dock_id);
+//		ResultSet result = statement.executeQuery(query);
+//		while (result.next()) {
+//			Bike bike = Bike.createBike(result.getInt("type"));
+//			bike.setImgUrl(result.getString("imgUrl"));
+//			bike.setType(result.getInt("type"));
+//			bike.setDockId(result.getInt("dockId"));
+//			bike.setBikeId(result.getInt("bikeId"));
+//			bike.setBarCode(result.getString("barCode"));
+//			bike.setLicensePlate(result.getString("licencePlate"));
+//			bike.setBeingUsed(result.getBoolean("isBeingUsed"));
+//
+//			// get other attribute of e-bike
+//			switch (result.getInt("bikeId")) {
+//			case StandardEBike.BIKE_TYPE_VALUE:
+//				getEBikeAttribute((StandardEBike) bike);
+//				break;
+//			default:
+//				break;
+//			}
+//
+//			bikeList.add(bike);
+//		}
+//
+//		return bikeList;
+//	}
 	
     public static Bike getBikeById(int bikeId) throws SQLException {
     	

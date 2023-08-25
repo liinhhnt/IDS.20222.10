1c27e4fd7d07ce767080d7bcdb5406172b18a332
linhpham39 <linhshark02@gmail.com>
2023-08-25 22:07:11 +0700

    update design

--- Programming/src/controller/return_bike/ReturnBikeController.java
+++ Programming/src/controller/return_bike/ReturnBikeController.java
@@ -1,7 +1,6 @@
 package controller.return_bike;
 
 import java.sql.SQLException;
-import java.time.LocalDateTime;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -11,9 +10,7 @@ import common.exception.InvalidSearchKeyException;
 import common.exception.NoResultException;
 import controller.BaseScreenController;
 import data_access_layer.bike.Bike_DAL;
-import data_access_layer.card.Card_DAL;
 import data_access_layer.dock.Dock_DAL;
-import data_access_layer.invoice.Invoice_DAL;
 import entity.bike.Bike;
 import entity.dock.Dock;
 import entity.invoice.Invoice;
@@ -35,44 +32,35 @@ public class ReturnBikeController extends BaseScreenController {
 
 	private final Card_DAL card_DAL = new Card_DAL();
 
-
-	private Dock dock;
-
-	public void setDock(Dock dock) {
-		this.dock = dock;
-	}
-
 	
 	//lấy ra invoice của cái xe định return
-	public Invoice getInvoice(Bike bike) throws SQLException  {	
-		return invoice_DAL.getRentInvoice(bike);
+	public Invoice getInvoice(int bikeId) throws SQLException  {	
+		return invoice_DAL.getRentInvoice(bikeId);
 	}
 //tính phí thuê xe
-
-	public int calculateFee(Invoice invoice, LocalDateTime start ,  LocalDateTime now) throws SQLException {
+	public int calculateFee(Invoice invoice) throws SQLException {
 		Bike bike = invoice.getBike();
 		ICalculator calculator = new CalculateFee();
-		int rentalTime = calculator.getTotalTime(start , now);
-		if(bike.getType() == 1){
-			return calculator.calculateStandardRentFee(rentalTime);
-		}else if(bike.getType() == 2){
-			return calculator.calculateElectricRentFee(rentalTime);
-		}else {
-			return calculator.calculateTwinRentFee(rentalTime);
+		if(bike.type == 1){
+			return calculator.calculateStandardRentFee(invoice.getTotalTime());
+		}else if(bike.type == 2){
+			return calculator.calculateElectricRentFee(invoice.getTotalTime());
+		}else if(bike.type == 3){
+			return calculator.calculateTwinRentFee(invoice.getTotalTime());
 		}
 	}
 
-	public void updateTotalTime(Invoice invoice, LocalDateTime currentTime) {
-		invoice.setTotalTime(currentTime);
+	public void updateTotalTime(Invoice invoice) {
+		invoice.setTotalTime();
 	}
 	public void updateTotalMoney(int fee, Invoice invoice) {
 		invoice.setTotalMoney(fee);
 	}
 
 	//update lại tình trạng xe, và tình trạng của dock
-	public void updateAfterReturnBike(int bikeId, int dockId) throws SQLException {
-		bike_DAL.updateReturnBikeStatus(bikeId, dockId ,utils.Constant.IS_NOT_USED);
-		dock_DAL.updateReturnDockPoint(dockId);
+	public void updateAfterReturnBike(int bikeId, int bikeType, int dockId) throws SQLException {
+		bike_DAL.updateBikeStatus(bikeId, utils.Constant.IS_NOT_USED);
+		dock_DAL.updateDockPoint(dockId);
 	}
 
 	
@@ -85,6 +73,8 @@ public class ReturnBikeController extends BaseScreenController {
 	//ấn trả xe, người dùng nhập card để thực hiện thanh toán (hoặc nhận lại tiền)
 	//check số dư của thẻ có lớn hơn fee ko
 	public boolean checkCardBalance(String cardReturn, int fee, String cardName, String exDate, String scCode){
-		return card_DAL.checkCardReturn(cardReturn, fee, cardName, exDate, scCode);
+		return card_DAL.checkCardBalance(cardReturn, fee, cardName, exDate, scCode);
 	}
+
+	
 }
